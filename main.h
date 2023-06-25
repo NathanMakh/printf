2c18850da908ca5a39fa0173ff3d83b072cebaad
NathanMakh <nathankutete@yahoo.com>
2023-06-25 23:40:31 +0000

    printf

--- main.h
+++ main.h
@@ -9,16 +9,4 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 void print_number(int n);
 
-struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-int print_char(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
 #endif
--- printf.c
+++ printf.c
@@ -1,10 +1,49 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10 != 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
 
 /**
  * _printf - produces output according to a format
  * @format: character string composed of zero or more directives
- *
- * Return: number of characters printed excluding null byte
+ * Return: the number of characters printed
  */
 
 int _printf(const char *format, ...)
@@ -32,6 +71,10 @@ int _printf(const char *format, ...)
 			case '%':
 				write(1, "%", 1);
 				count++;
+				break;
+			case 'd':
+			case 'i':
+				print_number(va_arg(args, int));
 				break;
 			default:
 				break;
