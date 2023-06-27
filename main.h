15f977c35e1bb4a4db56a04551045f5df63dedad
NathanMakh <nathankutete@yahoo.com>
2023-06-27 20:07:28 +0000

    main

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BUF_SIZE 1024
 
 int _putchar(char c);
 int _putnum(unsigned int num, unsigned int base);
 int _printf(const char *format, ...);
 int _putstr(const char *str);
+
 #endif
