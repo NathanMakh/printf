d61bde4344ab7f9b8804eb9c268fd3cf4c200fb8
NathanMakh <nathankutete@yahoo.com>
2023-06-27 14:14:09 +0000

    main.h

--- main.h
+++ main.h
@@ -1,9 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 
 int _putnum(unsigned int num, unsigned int base);
 int _printf(const char *format, ...);
